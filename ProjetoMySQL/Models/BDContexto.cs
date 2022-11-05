c27bc08e5ee5b1528cf3ed4e3df79e53c7816608
Adriano Ribeiro <adrianoribeiro589@gmail.com>
2022-11-05 17:32:37 -0300

    impletação eleitor funcionando

--- ProjetoMySQL/Models/BDContexto.cs
+++ ProjetoMySQL/Models/BDContexto.cs
@@ -25,7 +25,7 @@ namespace ProjetoMySQL.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;port=3306;user=arnmysql;password=cursomysql;database=AulaTeste", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.29-mysql"));
+                optionsBuilder.UseMySql("server=127.0.0.1;port=3306;user=arnmysql;password=cursomysql;database=AulaTeste", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.29-mysql"));
             }
         }
 
